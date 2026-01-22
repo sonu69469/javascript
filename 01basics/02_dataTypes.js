1b9dcdd77d210cf6fdb8592bdbd4b19d715ad322
sonu69469 <97663885+sonu69469@users.noreply.github.com>
2026-01-22 15:36:36 +0000

    added progress for dataTypes

---
+++ 01basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // it won't show any alert because we are using nodejs, not browser
+
+console.log(3 
+    +
+     9) // code readability should be high so it's suggested not to write code with extra spacing like this
+
+console.log("Sonu")
+
+
+let name = "Sonu"
+let age = 18
+let isLoggedIn = false
+let state;
+
+                          /* BELOW ARE DATATYPES--*/
+// number => upto 2 to the power 53
+// bigint => for numbers greated than 2^53
+// string => ""
+// boolean => true/false
+// null => it's a data type as well as a standlone value
+// undefined => a variable that is declared but not assigned any value
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof "sonu"); // string
+console.log(typeof 18); // number
+console.log(typeof age); // number
+console.log(typeof name); // string
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof state); // undefined as the value was not assigned
+
